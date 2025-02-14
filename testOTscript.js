e5bac9c8eac55cb6bc3373511b55ec92fd8484f8
albertolujan <63430621+albertolujan@users.noreply.github.com>
2025-02-14 10:29:06 +0000

    Update testOTscript.js

--- testOTscript.js
+++ testOTscript.js
@@ -1,20 +1,19 @@
 (function () {  
-            
-            var id = window["@xweb/core-utils/src/util/templateVariable/json"].pageMetaAttributes.userId
-			
-			var root = document.getElementById("container");
+    var id = window["@xweb/core-utils/src/util/templateVariable/json"].pageMetaAttributes.userId
 
-            var x = root.children[0].children[0].children[0].pageMeta.utilityLinks.label.altText // looking for "USER1 on behalf of USER2"
-        
-            if (x.includes("on behalf") || x.includes("en representaciÃ³n de") || x.includes("in Vertretung von") || x.includes("pour le compte de") || x.includes("per conto di"))    {
-                //sap.m.MessageBox.alert("Proxy detected. Please disable it and try again.");
-				alert("Proxy detected. Please disable it and try again.");
-            }
-            else {
-                //sap.m.MessageBox.alert("Loading OpenText in a new tab...");
-                //window.open("https://opentext-integration.euipo.europa.eu/OTCS/cs.exe/app/nodes/"+id+"_payslip", "_blank");
-		window.open("https://opentext-integration.euipo.europa.eu/OTCS/cs.exe/Open/RedirectToMainPayslip?userId="+id, "_blank");
-                
-            }
-                   
+    var x = window.pageHeaderJsonData.utilityLinks.label.altText // looking for "USER1 on behalf of USER2"
+
+
+    if (x.includes("on behalf") || x.includes("en representación de") || x.includes("in Vertretung von") || x.includes("pour le compte de") || x.includes("per conto di"))    {
+
+        //sap.m.MessageBox.alert("Proxy detected. Please disable it and try again.");
+
+        alert("Proxy detected. Please disable it and try again.");
+
+    }else {
+
+        //sap.m.MessageBox.alert("Loading OpenText in a new tab...");        
+        //window.open("https://opentext-test.euipo.europa.eu/OTCS/cs.exe/app/nodes/"+id+"_ws", "_blank");
+        window.open("https://opentext-integration.euipo.europa.eu/OTCS/cs.exe/Open/RedirectToMainWorkspace?userId="+id, "_blank");
+    }                  
 })();
